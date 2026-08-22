42c944a4341954421b86e65a23fc90accf35d180
Krishan Kant Sharma <148529159+KrishanKant2256@users.noreply.github.com>
2026-08-22 22:15:02 +0530

    Time: 1 ms (99.81%), Space: 43 MB (85.07%) - LeetHub

--- 0389-find-the-difference/0389-find-the-difference.java
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -1,21 +1,14 @@
 class Solution {
+
     public char findTheDifference(String s, String t) {
-        int map[]=new int[26];
-        for(int i=0;i<s.length();i++)
-        {
-            map[s.charAt(i)-'a']++;
-        }
-        for(int i=0;i<t.length();i++)
-        {
-            map[t.charAt(i)-'a']--;
-        }
-        for(int i=0;i<26;i++)
-        {
-            if(map[i]!=0)
-            {
-                return (char)('a' + i);
-            }
-        }
-        return ' ';
-    }
+        
+        int num = 0;
+
+        for(int cs : s.toCharArray()) num -= cs;
+
+        for(int ct : t.toCharArray()) num += ct;
+
+        return (char) (num);
+        
+         }
 }
