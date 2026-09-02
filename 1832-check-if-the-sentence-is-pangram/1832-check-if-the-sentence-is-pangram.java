4d17b0a234a0854eea9c35c6535fd7d24557522e
Krishan Kant Sharma <148529159+KrishanKant2256@users.noreply.github.com>
2026-09-02 09:29:55 +0530

    Time: 0 ms (100%), Space: 42.8 MB (83.81%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,20 +1,11 @@
 class Solution {
     public boolean checkIfPangram(String sentence) {
-        if(sentence.length()<26){
-            return false;
-        }
-        int map[]=new int[26];
-        for(int i=0;i<sentence.length();i++)
-        {
-            map[sentence.charAt(i)-'a']++;
-        }
-        for(int num:map)
-        {
-            if(num<1)
-            {
+        for (char ch='a';ch<='z';ch++){
+            if (sentence.indexOf(ch)==-1){
                 return false;
             }
         }
         return true;
     }
 }
+   
